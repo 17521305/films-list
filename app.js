35f05e8a64c7ec16b41005531bdce0b7b2edaae4
17521305 <17521305@gm.uit.edu.vn>
2020-05-08 22:22:12 +0700

    'Publish'

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const postsRouter = require('./routes/posts');
+app.use('/posts',postsRouter);
+
+app.get('/',(req,res)=>{
+    res.send('We are at home');
+})
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true },()=>console.log('Connected to DB!')
+)
+app.listen(8000);
